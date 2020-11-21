f1b651e6800500de2faf413d0d35199b155bd37e
Alexander Roman <alex37888@gmail.com>
2020-11-20 21:27:47 -0800

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Alexander Roman Montiel on 11/20/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
